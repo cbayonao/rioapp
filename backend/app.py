fb4e2e928b7b31fa4d4ebef61e04866cd9b8bde8
Julian Sandoval <derhks@gmail.com>
2021-02-20 15:15:05 -0500

    fixed login method

--- backend/app.py
+++ backend/app.py
@@ -1,29 +1,55 @@
 import json
 import mechanicalsoup
 
+
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-def login_municipality_rionegro() -> str:
+def login_municipality_rionegro(user: str, password: str) -> dict:
     url = 'http://entidadesintegradas.co/Registro/faces/Index?60N5MP1O=1'
+    url_login_ok = 'http://entidadesintegradas.co/Registro/faces/Main.xhtml'
 
     try:
         browser = mechanicalsoup.StatefulBrowser()
         browser.open(url=url)
-        browser.get_current_page()
         browser.select_form()
 
         # fill-in the form
-        browser['login:_t61'] = '1032448740'
-        browser['login:j_idt65'] = 'c4m1l0'
+        browser['login:_t61'] = user
+        browser['login:j_idt65'] = password
+        browser.new_control(type='input',
+                            name='javax.faces.source',
+                            value='login:btnLogin')
+        browser.new_control(type='input',
+                            name='javax.faces.partial.event',
+                            value='click')
+        browser.new_control(type='input',
+                            name='javax.faces.partial.execute',
+                            value='login:btnLogin login:btnLogin')
+        browser.new_control(type='input',
+                            name='javax.faces.partial.render',
+                            value='login')
+        browser.new_control(type='input',
+                            name='javax.faces.behavior.event',
+                            value='click')
+        browser.new_control(type='input',
+                            name='javax.faces.partial.ajax',
+                            value='true')
 
         browser.submit_selected()
     except Exception as error:
-        return str(error)
+        return {
+            'status': 'fail',
+            'response': str(error)
+        }
 
-    return 'Success'
+    return {
+        'status': 'success' if browser.url == url_login_ok else 'fail',
+        'response': str(browser.page)
+    }
 
 
 def create_account_municipality_rionegro() -> str:
@@ -69,14 +95,13 @@ def create_account_municipality_rionegro() -> str:
 
 def mainpage_municipality_rionegro() -> str:
     try:
-        browser = mechanicalsoup.StatefulBrowser()
         url = 'http://entidadesintegradas.co/Registro/faces/Index?60N5MP1O=1'
+        browser = mechanicalsoup.StatefulBrowser()
         browser.open(url=url)
-        browser.get_current_page()
     except Exception as error:
         return str(error)
 
-    return 'Success'
+    return str(browser.page)
 
 
 @app.route('/', methods=['GET'])
@@ -99,11 +124,13 @@ def create_account():
     }
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    response = login_municipality_rionegro()
+@app.route('/login/<string:cc>/<string:password>', methods=['POST'])
+def login(cc: str, password: str) -> dict:
+    user = escape(cc)
+    password = escape(password)
+    response = login_municipality_rionegro(user=user, password=password)
 
     return {
-        'statusCode': 200 if response == 'Success' else 500,
-        'body': json.dumps(response)
+        'statusCode': 200 if response['status'] == 'success' else 401,
+        'body': json.dumps(response['response'])
     }
