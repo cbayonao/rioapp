1362a164c36e22c43abc81f0d47c0b3b7946a4d8
cbayonao <drcamilob@live.com>
2021-02-27 09:46:26 -0500

    Delete files

--- backend/passwd-strength/constraints/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    verifyPasswordLength: require('./verifyPasswordLength'),
-    verifyPasswordStrength: require('./verifyPasswordStrength')
-}
--- backend/passwd-strength/constraints/verifyPasswordLength.js
+++
@@ -1,8 +0,0 @@
-module.exports = password => {
-    if ( password.length < 6 ) {
-        return Promise.reject({
-            message: 'El password es demasiado corto'
-        })
-    }
-    return Promise.resolve('El password pasa la validacion');
-};
